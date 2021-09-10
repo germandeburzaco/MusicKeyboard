fe299e8ce167ab37ef794d0e7a63d1986ed18cc5
German <52635113+germandeburzaco@users.noreply.github.com>
2021-09-10 20:53:12 -0300

    Update app.js

--- app.js
+++ app.js
@@ -18,3 +18,27 @@ window.addEventListener('keydown', (e)=>{
         teclaSeleccionada[0].classList.remove("presionada");
       }, 100);
 })
+
+
+document.addEventListener('click', function (event) {
+    var teclaPresionada = event.target.innerHTML 
+    teclaPresionada=teclaPresionada.toLowerCase()
+   // console.log(teclaPresionada)
+
+    var teclaSeleccionada = document.getElementsByClassName(`tecla${teclaPresionada}`)
+    //console.log(teclaSeleccionada)
+
+    song = document.getElementById(teclaPresionada)
+   // console.log(song)
+    if (!song) return
+    song.currentTime = 0;
+    song.play();
+
+    teclaSeleccionada[0].classList.add("presionada");
+   
+    setTimeout(function() {
+        teclaSeleccionada[0].classList.remove("presionada");       
+      }, 100);
+
+
+}, false);
