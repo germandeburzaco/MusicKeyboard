5d5dcb30ff6a16cdc8e86363d0a2dd4e460fe6c4
German <52635113+germandeburzaco@users.noreply.github.com>
2021-09-10 19:18:29 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+window.addEventListener('keydown', (e)=>{
+//    console.log(e)
+    var teclaPresionada=e.key
+    teclaPresionada=teclaPresionada.toLowerCase()
+    console.log(teclaPresionada)
+
+    var teclaSeleccionada = document.getElementsByClassName(`tecla${teclaPresionada}`)
+    console.log(teclaSeleccionada)
+
+    song = document.getElementById(teclaPresionada)
+   // console.log(song)
+    if (!song) return
+    song.currentTime = 0;
+    song.play();
+
+    teclaSeleccionada[0].classList.add("presionada");
+    setTimeout(function() {
+        teclaSeleccionada[0].classList.remove("presionada");
+      }, 100);
+})
